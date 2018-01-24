b553a767ca36600c89750a27def0ec348088b87a
Emily Welsch <emwelsch@gmail.com>
2018-01-24 21:08:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+xo = "X", "O", " "
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def display_board(board)
+def display_board(xo,board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
